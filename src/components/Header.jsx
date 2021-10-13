166245cb491384acf05e8afbfd16506db74716fe
Akash Ratan Verma <vermaakashratan232@gmail.com>
2021-10-13 23:46:30 +0530

    navbar start

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="header">
+            <img src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon-logo"/>
+        </div>
+    );
+}
+
+export default Header;
