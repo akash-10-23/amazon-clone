006823c4f6a19d4684c87da897d3816d38edcc33
Akash Ratan Verma <vermaakashratan232@gmail.com>
2021-10-16 23:43:44 +0530

    Finish Navbar frontend

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 function Header() {
     return (
         <div className="header">
-            <img src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon-logo"/>
+            <img 
+                className="headLogo"
+                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt="amazon-logo"
+            />
+
+            <div className="headerSearch">
+                <input className="headerSearchInput" type="text" />
+                <SearchIcon className="headerSearchIcon"/>
+            </div>
+
+            <div className="headerNav">
+                <div className="headerOption">
+                    <span className="OptionLineOne">Hello Guest</span>
+                    <span className="OptionLineTwo">Sign In</span>
+                </div>
+
+                <div className="headerOption">
+                    <span className="OptionLineOne">Returns</span>
+                    <span className="OptionLineTwo">& Orders</span>
+                </div>
+
+                <div className="headerOption">
+                    <span className="OptionLineOne">Your</span>
+                    <span className="OptionLineTwo">Prime</span>
+                </div>
+
+                <div className="headerCart">
+                    <ShoppingCartIcon />
+                    <span className="OptionLineTwo headerCartCount">
+                        0
+                    </span>
+                </div>
+            </div>
+            
         </div>
     );
 }
