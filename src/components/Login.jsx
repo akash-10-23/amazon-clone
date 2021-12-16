72005f0df3695ee68e0a5306071a586c3303a691
Akash Ratan Verma <vermaakashratan232@gmail.com>
2021-12-16 14:02:59 +0530

    Login frontend complete

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Login() {
+    
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signIn = e => {
+        e.preventDefault();
+    };
+
+    const register = e => {
+        e.preventDefault();
+    };
+
     return (
         <div className='login'>
             <Link to="/">
@@ -11,6 +23,35 @@ function Login() {
                     alt=''
                 />
             </Link>
+
+            <div className='loginContainer'>
+                <h1>Sign In</h1>
+
+                <form>
+                    <h5>Email</h5>
+                    <input
+                        type="text"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)} 
+                    />
+
+                    <h5>Password</h5>
+                    <input
+                        type="password" 
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+
+                    <button type='submit' onClick={signIn} className='signInBtn'> Sign In</button>
+                </form>
+
+                <p>
+                    By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
+                    see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+                </p>
+
+                <button onClick={register} className='registerBtn'>Create an Amazon account</button>
+            </div>
         </div>
     );
 }
