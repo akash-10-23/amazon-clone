cf125157e9cfcff6faf807e2aad4c30ea5af4d6f
Akash Ratan Verma <vermaakashratan232@gmail.com>
2022-03-06 15:01:40 +0530

    deploy v3.0

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:5001/clone-cf8cb/us-central1/api"  //the API (cloud function) URL
+    baseURL: "https://us-central1-clone-cf8cb.cloudfunctions.net/api"
+    //"http://localhost:5001/clone-cf8cb/us-central1/api"  //the API (cloud function) URL for localhost Testing
 });
 
 export default instance;
