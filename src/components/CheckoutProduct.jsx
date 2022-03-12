90689629fc5e83971803ef13a4d03af4ca447323
Akash Ratan Verma <vermaakashratan232@gmail.com>
2022-03-12 16:27:13 +0530

    added screenshots and deploy v3.0.1

--- src/components/CheckoutProduct.jsx
+++ src/components/CheckoutProduct.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import { useStateValue } from '../StateProvider';
+import CurrencyFormat from 'react-currency-format';
 
 function CheckoutProduct(props) {
     const { id, title, price, image, rating, hideButton } = props;
@@ -24,8 +25,20 @@ function CheckoutProduct(props) {
             <div className='checkoutProductInfo'>
                 <p className='checkoutProductTitle'>{title}</p>
                 <p className='checkoutProductPrice'>
-                    <small>₹</small>
-                    <strong>{price}</strong>
+                    {/* <small>₹</small>
+                    <strong>{price}</strong> */}
+
+                    <CurrencyFormat 
+                        renderText={(value) => (
+                            <strong>{value}</strong>
+                        )}
+                        decimalScale={2}
+                        value={price} 
+                        displayType={"text"}
+                        thousandSeparator={true}
+                        prefix={"₹"}
+                    />
+                
                 </p>
                 <div className='checkoutProductRating'>
                     {Array(rating).fill().map((_, i) => (
